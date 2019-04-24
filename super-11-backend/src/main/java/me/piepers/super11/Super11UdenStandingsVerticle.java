78e6389e27e33f64a8ad5239687489b3f3b06a9c
Bas Piepers <bas@piepers.me>
2019-04-24 19:46:43 +0200

    Added slow poller for competition standings as a fallback solution if scores are not correctly updated.

--- super-11-backend/src/main/java/me/piepers/super11/Super11UdenStandingsVerticle.java
+++ super-11-backend/src/main/java/me/piepers/super11/Super11UdenStandingsVerticle.java
@@ -36,6 +36,7 @@ public class Super11UdenStandingsVerticle extends AbstractVerticle {
     private static final Integer THREE_MINUTES = 1000 * 180;
     private static final Integer FIFTEEN_MINUTES = 1000 * 900;
     private static final Integer TWENTY_FOUR_HOURS = 1000 * 3600 * 24;
+    private static final Integer TWO_HOURS = 1000 * 3600 * 2;
 
     // The cached "competition" which is the standings of our league. Is updated by a timer so reads may be "dirty".
     private Competition competition;
@@ -69,6 +70,7 @@ public class Super11UdenStandingsVerticle extends AbstractVerticle {
 
         rxVertx.setPeriodic(FIFTEEN_MINUTES, this::handleFifteenMinutesTimer);
         rxVertx.setPeriodic(TWENTY_FOUR_HOURS, this::handleDailyLookups);
+        rxVertx.setPeriodic(TWO_HOURS, this::handleTwoHoursLookups);
     }
 
     @Override
@@ -76,6 +78,7 @@ public class Super11UdenStandingsVerticle extends AbstractVerticle {
         // Cache a competition to begin with and cache/update the season contents.
         competitionService
                 .rxFetchLatestCompetitionStandings()
+                .doOnSuccess(competition -> LOGGER.debug("Fetched latest competition."))
                 .doOnSuccess(competition -> this.competition = competition)
                 .ignoreElement()
                 .andThen(rxVertx.fileSystem().rxExists(storagePath))
@@ -130,7 +133,7 @@ public class Super11UdenStandingsVerticle extends AbstractVerticle {
                 vertx
                         .fileSystem()
                         .rxReadFile(storagePath + seasonFile)
-                        .doOnSuccess(buffer -> LOGGER.debug("Successfully read contents from file."))
+                        .doOnSuccess(buffer -> LOGGER.debug("Successfully read season contents from file."))
                         .flatMap(buffer -> Single
                                 .just(new Season(buffer
                                         .toJsonObject())));
@@ -177,6 +180,22 @@ public class Super11UdenStandingsVerticle extends AbstractVerticle {
     private void handleFifteenMinutesTimer(Long timerId) {
         LOGGER.debug("Timer triggered with id {}", timerId);
         this.checkAndStartCompetitionPolling();
+    }
+
+    /**
+     * In some occasions, during the matches, the scores on the board are not displayed correctly. The ProfCoach API
+     * sometimes amends scores at a later stage after the match round. In order to reflect these scores, we poll at a
+     * slow rate to cache the latest competition standings.
+     *
+     * @param timerId, the id of the timer with which this method is called.
+     */
+    private void handleTwoHoursLookups(Long timerId) {
+        LOGGER.debug("Handling slower competition standings polling.");
+        if (Objects.nonNull(this.competitionPollTimerId)) {
+            LOGGER.debug("A faster poller is already active, don't poll again.");
+        } else {
+            this.handleCompetitionLookupTimer(timerId);
+        }
     }
 
     private void checkAndStartCompetitionPolling() {
